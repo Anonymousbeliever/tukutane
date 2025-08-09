24d51d9c7dbf29117dda1f2f361909ed69620428
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-08-09 18:56:36 +0300

    added payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'amount',
+        'type',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'amount' => 'decimal:2',
+    ];
+
+    /**
+     * Get the user that made the payment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the event associated with the payment.
+     */
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    /**
+     * Get the M-Pesa transaction associated with the payment.
+     */
+    public function mpesaTransaction()
+    {
+        return $this->hasOne(MpesaTransaction::class);
+    }
+}
