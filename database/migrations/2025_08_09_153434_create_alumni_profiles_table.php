65baa7df064b9aded519c00641c5b4a5f11e8484
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-08-09 18:35:34 +0300

    added code

--- database/migrations/2025_08_09_153434_create_alumni_profiles_table.php
+++ database/migrations/2025_08_09_153434_create_alumni_profiles_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('alumni_profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('phone_number')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('linkedin_url')->nullable();
+            $table->string('github_url')->nullable();
+            $table->string('current_company')->nullable();
+            $table->string('job_title')->nullable();
             $table->timestamps();
         });
     }
